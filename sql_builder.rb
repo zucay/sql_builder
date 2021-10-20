2f2bc6f8c33c6a4cc61500fa791456fef3ca50d0
zucay <>
2021-10-20 10:05:39 +0900

    basic function

--- sql_builder.rb
+++ sql_builder.rb
@@ -6,11 +6,14 @@ require 'pry'
 class FileNode
   attr_accessor :path, :parents, :lines
   def initialize(path)
-    @path = path
+    ab_path = File.expand_path(path)
+
+    @base_name = File.basename(path)
+    @path = ab_path
     @parents = []
     @lines = []
 
-    read_file(path)
+    read_file
   end
 
   def ancestors
@@ -28,101 +31,54 @@ class FileNode
     return false
   end
 
-  def read_file
-    fail # subclass
-  end
-end
-
-class TextNode < FileNode
-  def read_file
-    open(@path).read.each do |line|
-      if line =~ /^--\s*with_import\s['"](.*)['"]/
-        @parents << WithElement.new($1)
-      else
-        out << line
-      end
-    end
-  end
-end
-
-class WithElement < TextNode
-end
-
-class MainText < TextNode
-end
-
-=begin
-class FileImportable
-  def initialize(path, import_regex = /^--\s*with_import\s['"](.*)['"]/)
-    @import_regex = import_regex    
-    @lines = read(path)
-  end
-  def read(file)
-    out = []
-    open(file).each do |line|
-      if line =~ @import_regex
-        read($1)
-      else
-        out << line
-      end
-    end
-    out.flatten
-  end
   def render
-    out = @lines.join("\n")
-    `echo #{out} | pbcopy` # 出力したものをクリップボードに保存　
+    output_path = "#{Time.now.strftime('%y%m%d_%H%m')}_#{@base_name}.sql"
+    fo = open(output_path, 'w')
+    fo.write(build)
+    fo.close
+    `pbcopy < #{output_path}`
+  end
+
+  # need to implement on subclass
+  def read_file
+    fail
+  end
+
+  def build
+    fail
   end
 end
 
-class SQLBuilder
-  @@with_first_time = true
-
-  def self.with sql_file
-    if @@with_first_time
-      print 'WITH　'
-    else
-      print ', '
-    end
-
-    print open(sql_file).read
-    @@with_first_time = false
-  end
-
-  def self.import sql_file
-    open(sql_file).read
-  end
-
-  def parse_file(sql_file)
-    out = []
-    fi = open(sql_file).read
-
-    fi.each do |line|
-      if line =~ /^-- with_import ''/
+class SQLTextNode < FileNode
+  def read_file
+    open(@path).each do |line|
+      if line =~ /^--\s*with_import\s['"](.*)['"]/
+        @parents << SQLWithClause.new($1)
+      else
+        @lines << line
       end
     end
-    out.flatten
   end
 
-  def self.execute(sql_file)
-    dir, base, ext = [File.dirname(sql_file), File.basename(sql_file), File.extname(sql_file)]
-    Dir.chdir(dir)
-    rendered_file = File.join(dir, "out_#{base}#{ext}")
-    @@fo = File.open(rendered_file, 'w')
-    @@fo.print "-- THIS FILE IS RENDERED BY SQL_BUILDER https://github.com/zucay/sql_builder\n"
-    @@fo.print "-- #{Time.now.to_s}\n"
-    @@fo.print "\n"
-
-    lines = self.parse_file(sql_file)
-
-    lines.each do |line|
-      @@fo.print line
-    end
-    @@fo.close
+  def build
+    head = ancestors.map do |ancestor|
+      ancestor.lines.join
+    end.join(", \n")
+    out = "WITH #{head} \n" + @lines.join
+    out
   end
+
 end
-=end
+
+class SQLWithClause < SQLTextNode
+end
+
+class SQLMainText < SQLTextNode
+end
+
 
 if __FILE__ == $0
   fail 'set file' unless ARGV[0]
-  # SQLBuilder.execute(ARGV[0])
+  node = SQLMainText.new(ARGV[0])
+  node.render
 end
